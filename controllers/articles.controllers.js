ec87c6cc5e87b816486649c4331e750673707429
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2023-11-20 14:29:42 +0000

    begin to add functionality to get all articles

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,4 +1,7 @@
-const { selectArticle } = require("../models/articles.models");
+const {
+  selectArticle,
+  selectAllArticles,
+} = require("../models/articles.models");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
@@ -7,4 +10,11 @@ exports.getArticleById = (req, res, next) => {
       res.status(200).send({ article });
     })
     .catch(next);
+};
+
+exports.getArticles = (req, res, next) => {
+  selectAllArticles().then((articles) => {
+    console.log(articles)
+    res.status(200).send({ articles });
+  });
 };
