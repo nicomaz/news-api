475066b0d6f7134c30470881a2ec3fcedf877bfe
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2023-11-20 11:32:42 +0000

    add server error handling

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
+const { handleServerErrors } = require("./errors");
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.use(handleServerErrors);
 
 module.exports = app;
