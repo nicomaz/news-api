e8f6b92fd9aa477f150596ea71a37895d3e83a90
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2023-11-21 12:16:15 +0000

    getCommentsByArticleId: refactor

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -3,11 +3,7 @@ const { selectCommentsByArticleId } = require("../models/comments.models");
 
 exports.getCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
-  const articlePromises = [selectCommentsByArticleId(article_id)];
-
-  if (article_id) {
-    articlePromises.push(checkExists("articles", "article_id", article_id));
-  }
+  const articlePromises = [selectCommentsByArticleId(article_id), checkExists("articles", "article_id", article_id)];
 
   Promise.all(articlePromises)
     .then((resolvedPromises) => {
