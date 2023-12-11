4da0296f0f2183e25ec76fe61b81d03b313775d3
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2023-12-11 10:06:27 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const { handleServerErrors, handlePsqlErrors, handleCustomErrors } = require("./errors");
 const { handle404 } = require("./controllers/api.controllers");
+const cors = require('cors');
 
 const express = require("express");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const apiRouter = require("./routes/api-router");
